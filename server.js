8fe2cd025a8d5376c84f74d21dac2750cc66b66e
robinbaud <72597879+robinbaud@users.noreply.github.com>
2022-11-16 15:38:16 +0100

    add dockerfile and basic express application

---
+++ server.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const express = require("express");
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
